1073343b1c26da71d968e008333f45d2de081083
Wolfgang Jährling <wolfgang@conseptizer.org>
2018-04-28 22:19:22 +0200

    nicer message

--- okami.s
+++ okami.s
@@ -270,7 +270,7 @@
   welcome_message: @ starts with system message
     .ascii "\033[33;1msystem\033[0m: "
     .equ system_response_size, . - system_response
-    .ascii "starting version 0.0 "
+    .ascii "version 0.0 welcome "
     .equ welcome_message_size, . - welcome_message
   goodbye_message:
     .ascii "bye\n"
