a12575b1d9990ff5c5471cef1c4934a90ec7c2c8
Wolfgang Jährling <wolfgang@okami-systems.org>
2019-05-09 15:33:42 +0200

    extend typechecking

--- wok.scm
+++ wok.scm
@@ -5,26 +5,37 @@
                (= (int int) (bool))
                (not (bool) (bool))))
 
-(define (error . text)
+(define (fail)
+  (eval '(#f)))
+
+(define (say . text)
   (for-each display text)
-  (newline)
-  (#f))
+  (newline))
+
+(define (error . text)
+  (apply say text)
+  (fail))
 
 (define initial '())
 
 (define (initial+ t)
+  (say "init:" t)
   (set! initial (cons t initial)))
 
 (define current '(int))
 
+(define (set-current! types)
+  (say "new:" types)
+  (set! current types))
+
 (define (current+ t)
-  (set! current (cons t current)))
+  (set-current! (cons t current)))
 
 (define (current- t)
   (if (null? current)
       (initial+ t)
       (if (eq? (car current) t)
-          (set! current (cdr current))
+          (set-current! (cdr current))
           (error "requested " t "but having " (car current)))))
 
 (define (apply-effect-of op)
@@ -38,7 +49,52 @@
 (define (current-multi- types)
   (for-each current- types))
 
-(apply-effect-of '=)
+(define (infer code)
+  (for-each (lambda (element)
+              (if (symbol? element)
+                  (apply-effect-of element)
+                  (apply-structure-effect element)))
+            code))
+
+(define (apply-structure-effect struct)
+  (case (car struct)
+    ((if) (begin
+            (current- 'bool)
+            (let ((prev current))
+              (infer (cadr struct))
+              (let ((t-branch current))
+                (if (not (null? (cddr struct)))
+                    (begin
+                      (set-current! prev)
+                      (infer (caddr struct))
+                      (if (compatible-branches? t-branch current)
+                          (possibly-extend-to t-branch)
+                          (error "incompatible branches from " prev " to "
+                                 t-branch " vs. " current))))))))
+    ((loop) (fail))))
+
+(define (compatible-branches? variant1 variant2)
+  (or (includes? variant1 variant2)
+      (includes? variant2 variant1)))
+
+(define (possibly-extend-to result)
+  (if (includes? result current)
+      (set-current! result)))
+
+(define (includes? large small)
+  (prefix? (reverse small) (reverse large)))
+
+(define (prefix? prefix xs)
+  (cond ((null? prefix) #t)
+        ((null? xs) #f)
+        ((not (type= (car prefix) (car xs))) #f)
+        (else (prefix? (cdr prefix) (cdr xs)))))
+
+(define (type= t1 t2)
+  (eq? t1 t2)) ; for now
+
+(infer '(= (if (+) (+))))
+;(infer '(= (if (+) (drop-int))))
 
 
 (display initial)
