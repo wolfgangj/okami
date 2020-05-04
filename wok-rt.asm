be865269af1a16fa26088158429324a33e7a2144
Wolfgang Jährling <wolfgang@okami-systems.org>
2020-05-04 13:23:04 +0200

    remove gdata hack, no longer needed

--- wok-rt.asm
+++ wok-rt.asm
@@ -49,9 +49,6 @@ obj_stack_top:
 orig_rsp:
         resq 1
 
-gdata:
-        resq 1
-
 section .text
 
 global runtime.outofbounds
@@ -84,20 +81,6 @@ runtime.syscall:
         pop rdi
         pop rsi
         add rbp, 48
-        ret
-
-global runtime.setgdata
-runtime.setgdata:
-        mov [gdata], rax
-        mov rax, [rbp+8]
-        add rbp, 8
-        ret
-
-global runtime.getgdata
-runtime.getgdata:
-        mov [rbp], rax
-        sub rbp, 8
-        mov rax, [gdata]
         ret
 
 global runtime.getarg
