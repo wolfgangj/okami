0c8dbb80ba4f8e589ff7a740885028a80fade4b9
Wolfgang Jährling <wolfgang@okami-systems.org>
2020-05-07 20:58:35 +0200

    turn asm templates into macros

--- asm-test/app.asm
+++ asm-test/app.asm
@@ -1,5 +1,4 @@
-default rel
-extern runtime.outofbounds
+%include "instructions.asm"
 
 ; included via runtime.wok
 extern runtime.syscall
@@ -7,12 +6,14 @@ extern runtime.getarg
 extern runtime.outofbounds ; duplicate
 
 section .rodata
-.L1: db 'hello',10,0
+L0: db 'hello',10,0
 
 section .text
 
-;global app._size
-;app._size equ 16 ; how large an instance is
+; class:
+global app._size
+app._size equ 16 ; how large an instance is
+app.new:
 
 global run
 run:
@@ -23,7 +24,7 @@ mov rax, 2 ; stdout-fd
 
 mov [rbp], rax
 sub rbp, 8
-lea rax, [.L1]
+lea rax, [L0]
 
 mov [rbp], rax
 sub rbp, 8
@@ -49,3 +50,41 @@ call runtime.syscall
 call runtime.outofbounds
 
 ret
+
+wok_drop
+wok_nip
+wok_this
+wok_that
+wok_them
+wok_alt
+wok_tuck
+wok_dropem
+wok_method_call app.new
+wok_self
+wok_attr_access 8
+wok_add
+wok_sub
+wok_mul
+wok_div
+wok_mod
+wok_not
+wok_and
+wok_or
+wok_xor
+wok_ashift_right
+wok_shift_right
+wok_shift_left
+wok_idx 10, 8
+wok_if_check L1
+wok_if_end L1
+wok_eif_check L2
+wok_eif_else L3, L2
+wok_eif_end L3
+wok_ehas_check L4
+wok_ehas_else L5, L4
+wok_ehas_end L5
+wok_loop_start L6
+wok_break L7
+wok_loop_end L6, L7
+wok_new_start app
+wok_new_end
--- asm-test/instructions.asm
+++ asm-test/instructions.asm
@@ -1,301 +1,365 @@
 default rel
 extern runtime.outofbounds
 
-section .text
-
 ;;;;;;;;;;;;;;;; stack ops
 
-drop:
+%macro wok_drop 0
         mov rax, [rbp+8]
         add rbp, 8
+%endmacro
 
-nip:
+%macro wok_nip 0
         add rbp, 8
+%endmacro
 
-this:
+%macro wok_this 0
         mov [rbp], rax
         sub rbp, 8
+%endmacro
 
-that:
+%macro wok_that 0
         mov rdx, [rbp+8]
         mov [rbp], rdx
         sub rbp, 8
+%endmacro
 
-them:
+%macro wok_them 0
         mov rdx, [rbp+8]
         mov [rbp], rax
         mov [rbp-8], rdx
         sub rbp, 16
+%endmacro
 
-alt:
+%macro wok_alt 0
         mov rdx, [rbp+8]
         mov [rbp+8], rax
         mov rax, rdx
+%endmacro
 
-tuck:
+%macro wok_tuck 0
         mov rdx, [rbp+8]
         mov [rbp+8], rax
         mov [rbp], rdx
         sub rbp, 8
+%endmacro
 
-dropem:
+%macro wok_dropem 0
         mov rax, [rbp+24]
         add rbp, 16
+%endmacro
 
 ;;;;;;;;;;;;;;;; classes
 
-method_call:
+%macro wok_method_call 1 ; method-name
         mov [rsi], rdi          ; move object from data to object stack
         mov rdi, rax
         mov rax, [rbp+8]
         add rbp, 8
         sub rsi, 8
 
-        call that               ; the method
+        call %1                 ; the method
 
         mov rdi, [rsi+8]
         add rsi, 8
+%endmacro
 
-self:
+%macro wok_self 0
         mov [rbp], rax          ; copy from obj stack to data stack
         sub rbp, 8
         mov rax, rdi
+%endmacro
 
-attr_access:
-        add rax, 8              ; offset of attribute in object
+%macro wok_attr_access 1 ; offset
+        add rax, %1             ; offset of attribute in object
+%endmacro
 
 ;;;;;;;;;;;;;;;; math
 
-add:
+%macro wok_add 0
         add rax, [rbp+8]
         add rbp, 8
+%endmacro
 
-sub:
+%macro wok_sub 0
         mov rdx, [rbp+8]
         add rbp, 8
         sub rdx, rax
         mov rax, rdx
+%endmacro
 
-mul:
+%macro wok_mul 0
         imul qword [rbp+8]
         add rbp, 8
+%endmacro
 
-div:
+%macro wok_div 0
         idiv qword [rbp+8]
         add rbp, 8
+%endmacro
 
-mod:
+%macro wok_mod 0
         idiv qword [rbp+8]
         add rbp, 8
         mov rax, rdx
+%endmacro
 
 ;;;;;;;;;;;;;;;; logic
 
-not:
+%macro wok_not 0
         not rax
+%endmacro
 
-and:
+%macro wok_and 0
         and rax, [rbp+8]
         add rbp, 8
+%endmacro
 
-or:
+%macro wok_or 0
         or rax, [rbp+8]
         add rbp, 8
+%endmacro
 
-xor:
+%macro wok_xor 0
         xor rax, [rbp+8]
         add rbp, 8
+%endmacro
 
 ;;;;;;;;;;;;;;;; shifts
 
-ashift_right:
+%macro wok_ashift_right 0
         sarx rax, [rbp+8], rax
         add rbp, 8
+%endmacro
 
-shift_right:
+%macro wok_shift_right 0
         shrx rax, [rbp+8], rax
         add rbp, 8
+%endmacro
 
-shift_left:        
+%macro wok_shift_left 0
         shlx rax, [rbp+8], rax
         add rbp, 8
+%endmacro
 
 ;;;;;;;;;;;;;;;; array index
 
-idx:
+%macro wok_idx 2 ; array-elements element-size
         mov rdx, [rbp+8]
         add rbp, 8
           ; oob check:
-          cmp rdx, 20 ; size of array
-          jae runtime.outofbounds ; unsigned, so we only have to check upper
-        lea rax, [rax+rdx*8] ; size of element
+          cmp rdx, %1                   ; size of array
+          jae runtime.outofbounds       ; unsigned, so only checking upper
+        lea rax, [rax+rdx*%2]           ; size of element
+%endmacro
 
 ;;;;;;;;;;;;;;;; control structures
 
-if_check:
+%macro wok_if_check 1 ; end-label
         add rbp, 8
         test rax, rax
         mov rax, [rbp+8]
-        jz end_if
-        ;;; code of then_branch
-end_if:
+        jz %1
+%endmacro
 
-eif_check:
+%macro wok_if_end 1 ; end-label
+        %1:
+%endmacro
+
+%macro wok_eif_check 1 ; else-label
         add rbp, 8
         test rax, rax
         mov rax, [rbp+8]
-        jz else_branch
-        ;;; code of then_branch
-        jmp end_eif
-else_branch:
-        ;;; code of else-branch
-end_eif:
+        jz %1
+%endmacro
 
-ehas_check:
+%macro wok_eif_else 2 ; end-label else-label
+        jmp %1
+        %2:
+%endmacro
+
+%macro wok_eif_end 1 ; end-label
+        %1:
+%endmacro
+
+%macro wok_ehas_check 1 ; else-label
         test rax, rax
-        jz ehas_else
-        ;;; code of then-branch
-ehas_else:
+        jz %1
+%endmacro
+
+%macro wok_ehas_else 2 ; end-label else-label
+        jmp %1
+        %2:
         mov rax, [rbp+8] ; drop1
         add rbp, 8       ; drop2
-        ;;; code of else-branch
-end_ehas:
+%endmacro
 
-loop:
-        ;; body of loop
-break:
-        jmp end_of_loop
-        ;; more body of loop
-jmp loop
-end_of_loop:
+%macro wok_ehas_end 1 ; end-label
+       %1:
+%endmacro
+
+%macro wok_loop_start 1 ; start-label
+        %1:
+%endmacro
+
+; in combination with 'new', we have to insert exit code for 'new' before this
+%macro wok_break 1 ; end-label
+        jmp %1
+%endmacro
+
+%macro wok_loop_end 2 ; start-label end-label
+        jmp %1
+        %2:
+%endmacro
 
 ;;;;;;;;;;;;;;;; mem ops
 
-at_64:
+%macro wok_at_64 0
         mov rax, [rax]
+%endmacro
 
-at_s32:
+%macro wok_at_s32 0
         movsxd rax, [rax]
+%endmacro
 
-at_s16:
+%macro wok_at_s16 0
         movsx rax, word [rax]
+%endmacro
 
-at_s8:
+%macro wok_at_s8 0
         movsx rax, byte [rax]
+%endmacro
 
-at_u8:
+%macro wok_at_u8 0
         movzx rax, byte [rax]
+%endmacro
 
-at_u16:
+%macro wok_at_u16 0
         movzx rax, word [rax]
+%endmacro
 
-at_u32:
+%macro wok_at_u32 0
         mov eax, dword [rax] ; this will clear the upper 32 bit
+%endmacro
 
-store_64:
+%macro wok_store_64 0
         mov rdx, [rbp+8]
         add rbp, 16
         mov [rax], rdx
         mov rax, [rbp]
+%endmacro
 
-store_32:
+%macro wok_store_32 0
         mov edx, [rbp+8]
         add rbp, 16
         mov [rax], edx
         mov rax, [rbp]
+%endmacro
 
-store_16:
+%macro wok_store_16 0
         mov dx, [rbp+8]
         add rbp, 16
         mov [rax], dx
         mov rax, [rbp]
+%endmacro
 
-store_8:
+%macro wok_store_8 0
         mov dl, [rbp+8]
         add rbp, 16
         mov [rax], dl
         mov rax, [rbp]
+%endmacro
 
 ;;;;;;;;;;;;;;;; relational ops
 
-is_eq:
+%macro wok_is_eq 0
         xor edx, edx
         cmp rax, [rbp+8]
         setnz dl
         mov rax, rdx
         dec rax
         add rbp, 8
+%endmacro
 
-is_neq:
+%macro wok_is_neq 0
         xor edx, edx
         cmp rax, [rbp+8]
         setz dl
         mov rax, rdx
         dec rax
         add rbp, 8
+%endmacro
 
-is_l:
+%macro wok_is_l 0
         xor edx, edx
         cmp rax, [rbp+8]
         setnl dl
         mov rax, rdx
         dec rax
         add rbp, 8
+%endmacro
 
-is_ge:
+%macro wok_is_ge 0
         xor edx, edx
         cmp rax, [rbp+8]
         setl dl
         mov rax, rdx
         dec rax
         add rbp, 8
+%endmacro
 
-is_g:
+%macro wok_is_g 0
         xor edx, edx
         cmp rax, [rbp+8]
         setng dl
         mov rax, rdx
         dec rax
         add rbp, 8
+%endmacro
 
-is_le:
+%macro wok_is_le 0
         xor edx, edx
         cmp rax, [rbp+8]
         setg dl
         mov rax, rdx
         dec rax
         add rbp, 8
+%endmacro
 
 ;;;;;;;;;;;;;;;; allocation
 
-new:
+; TODO: this can be optimized, we don't need to push rsp
+%macro wok_new_start 1 ; class-name
         mov [rsi], rdi  ; make space in top of obj stack
         sub rsi, 8
 
         push rsp        ; so that we can restore later
 
-        mov rdx, 16     ; actually classname._size
+        mov rdx, %1._size
         sub rsp, rdx    ; allocate
         mov rdi, rsp    ; set top of obj stack
 
-        call that       ; classname.new
+        call %1.new
 
         mov [rbp], rax  ; move top of data stack to top of object stack
         mov rax, rdi
         mov rdi, [rsi+8]
         add rsi, 8
         sub rbp, 8
+%endmacro
 
-        ;; code in new_block
-
+%macro wok_new_end 0
         pop rsp         ; restore original call stack
+%endmacro
 
 ;;;;;;;;;;;;;;;; tagged unions
 
-is:
+%macro wok_is 2 ; tag-value end-label
         mov rdx, [rax]
-        cmp rdx, 3      ; tag value
-        jne end_is
+        cmp rdx, %1      ; tag value
+        jne %2
         add rax, 8
-        ;; code_of_block, usually ends with 'ok'/ret
-end_is:
+%endmacro
+
+%macro wok_is_end 1 ; end-label
+        %2:
+%endmacro
