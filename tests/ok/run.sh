161f54e87b56cb1a0c532e57ac246f39235c295c
Wolfgang Jährling <wolfgang@okami-systems.org>
2020-05-31 19:44:49 +0200

    add first testcases

---
+++ tests/ok/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+nasm -f elf64 ../../runtime/wok-rt.asm -o wok-rt.o || exit 1
+nasm -f elf64 ../../runtime/os-openbsd.asm -o os.o || exit 1
+
+for f in ok-*.wok; do
+    MODULE=$(echo "$f" | sed -e 's/.wok$//')
+    ruby ../../wok.rb "$MODULE" >"$MODULE".asm || exit 1
+    nasm -f elf64 "$MODULE".asm -o "$MODULE".o || exit 1
+    ld.bfd -o test-ok -nostdlib -pie -static -z notext wok-rt.o os.o "$MODULE".o || exit 1
+    ./test-ok || (echo "$f" failed; exit 1)
+done
