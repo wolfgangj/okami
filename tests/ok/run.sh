f33ecb0712d8861b831c81254bf6d2215a33b914
Wolfgang Jährling <wolfgang@okami-systems.org>
2020-05-31 19:48:24 +0200

    setup .gitignore for tests

--- tests/ok/run.sh
+++ tests/ok/run.sh
@@ -6,6 +6,6 @@ for f in ok-*.wok; do
     MODULE=$(echo "$f" | sed -e 's/.wok$//')
     ruby ../../wok.rb "$MODULE" >"$MODULE".asm || exit 1
     nasm -f elf64 "$MODULE".asm -o "$MODULE".o || exit 1
-    ld.bfd -o test-ok -nostdlib -pie -static -z notext wok-rt.o os.o "$MODULE".o || exit 1
-    ./test-ok || (echo "$f" failed; exit 1)
+    ld.bfd -o "test-$MODULE" -nostdlib -pie -static -z notext wok-rt.o os.o "$MODULE".o || exit 1
+    ./test-"$MODULE" || (echo "$f" failed; exit 1)
 done
