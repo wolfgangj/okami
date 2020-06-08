7528c2e3779646c62b075e6951b1e91c6f814016
Wolfgang Jährling <wolfgang@okami-systems.org>
2020-06-08 17:57:38 +0200

    wbat: update build script

--- apps/wbat/build.sh
+++ apps/wbat/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-nasm -f elf64 ../../runtime/wok-rt.asm -o wok-rt.o || exit 1
-nasm -f elf64 ../../runtime/os-openbsd.asm -o os.o || exit 1
+nasm -f elf64 -dOS=openbsd ../../runtime/wok-rt-amd64.asm -o wok-rt.o || exit 1
+nasm -f elf64 ../../runtime/os-openbsd-amd64.asm -o os.o || exit 1
 
 ruby ../../wok.rb wbat >wbat.asm || exit 1
 nasm -f elf64 wbat.asm -o wbat.o || exit 1
