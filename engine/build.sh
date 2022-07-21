1bac57fef09e1ef47669238f5800c161bc974bc0
Wolfgang Jährling <wolfgang@okami-systems.org>
2022-07-21 11:12:22 +0200

    engine: make build script display settings

--- engine/build.sh
+++ engine/build.sh
@@ -15,7 +15,12 @@ if [ "$ARCH" = x86_64 ]; then
     ARCH=amd64
 fi
 
+echo "OS:      $OS"
+echo "ARCH:    $ARCH"
+echo "LDFLAGS: $LDFLAGS"
+
 nasm -f elf64 -dOS="$OS" okami-"$ARCH".s -o okami.o || exit 1
+#ld.lld -o okami -nostdlib $LDFLAGS okami.o || exit 1
 ld.bfd -o okami -nostdlib $LDFLAGS okami.o || exit 1
 
 if [ "$1" = '-d' ]; then
