97729920fe25de34044210f68afb1b5d479163f0
Wolfgang Jährling <wolfgang@okami-systems.org>
2023-04-18 14:26:05 +0200

    forth: fix minor oversight

--- forth/okcc.sh
+++ forth/okcc.sh
@@ -3,7 +3,7 @@
 
 while read -r line; do
     if [ -n "$(printf "%s\n" "$line" | grep -E '^{.+:.+}$')" ]; then
-        echo $(sh ../propulsion/const.sh $(echo "$line" | sed -E -e 's/\{//' -e 's/}//' -e 's/:/.h /'))
+        echo $(sh ../forth/const.sh $(echo "$line" | sed -E -e 's/\{//' -e 's/}//' -e 's/:/.h /'))
     else
         printf "%s\n" "$line"
     fi
